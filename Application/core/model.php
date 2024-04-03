48a9af0e93c56f5dd3c26a7ed02d72fe9752f4ee
dmsakhnov <dm.sakhnov@gmail.com>
2024-04-03 07:33:07 +0700

    update project

--- Application/core/model.php
+++ Application/core/model.php
@@ -2,7 +2,11 @@
 class Model
 {
     public $string;
-    public function __construct(){
+    public function __construct() {
         $this->string = "MVC + PHP = Awesome!";
+    }
+
+    public function get_data() {
+        
     }
 }
